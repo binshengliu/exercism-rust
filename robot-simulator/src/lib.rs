6cb28d105ea93b543187d5b16df5c133c2019dd0
Binsheng Liu <lbsx@users.noreply.github.com>
2017-09-15 14:43:07 +0800

    impl Direction

--- robot-simulator/src/lib.rs
+++ robot-simulator/src/lib.rs
@@ -9,6 +9,26 @@ pub enum Direction {
     West,
 }
 
+impl Direction {
+    pub fn turn_right(&mut self) {
+        *self = match *self {
+            Direction::North => Direction::East,
+            Direction::East => Direction::South,
+            Direction::South => Direction::West,
+            Direction::West => Direction::North,
+        };
+    }
+
+    pub fn turn_left(&mut self) {
+        *self = match *self {
+            Direction::North => Direction::West,
+            Direction::East => Direction::North,
+            Direction::South => Direction::East,
+            Direction::West => Direction::South,
+        };
+    }
+}
+
 pub struct Robot {
     x: isize,
     y: isize,
@@ -24,51 +44,38 @@ impl Robot {
         }
     }
 
-    pub fn turn_right(self) -> Self {
-        let direction = match self.direction {
-            Direction::North => Direction::East,
-            Direction::East => Direction::South,
-            Direction::South => Direction::West,
-            Direction::West => Direction::North,
-        };
-
-        Robot::new(self.x, self.y, direction)
+    pub fn turn_right(mut self) -> Self {
+        self.direction.turn_right();
+        self
     }
 
-    pub fn turn_left(self) -> Self {
-        let direction = match self.direction {
-            Direction::North => Direction::West,
-            Direction::East => Direction::North,
-            Direction::South => Direction::East,
-            Direction::West => Direction::South,
-        };
-
-        Robot::new(self.x, self.y, direction)
+    pub fn turn_left(mut self) -> Self {
+        self.direction.turn_left();
+        self
     }
 
-    pub fn advance(self) -> Self {
-        let (x, y) = match self.direction {
-            Direction::North => (self.x, self.y + 1),
-            Direction::East => (self.x + 1, self.y),
-            Direction::South => (self.x, self.y - 1),
-            Direction::West => (self.x - 1, self.y),
+    pub fn advance(mut self) -> Self {
+        match self.direction {
+            Direction::North => self.y += 1,
+            Direction::East => self.x += 1,
+            Direction::South => self.y -= 1,
+            Direction::West => self.x -= 1,
         };
 
-        Robot::new(x, y, self.direction)
+        self
     }
 
-    pub fn instructions(self, instructions: &str) -> Self {
-        let mut robot = self;
+    pub fn instructions(mut self, instructions: &str) -> Self {
         for c in instructions.chars() {
-            robot = match c {
-                'A' => robot.advance(),
-                'L' => robot.turn_left(),
-                'R' => robot.turn_right(),
-                _ => robot
-            }
+            self = match c {
+                'A' => self.advance(),
+                'L' => self.turn_left(),
+                'R' => self.turn_right(),
+                _ => self
+            };
         }
 
-        robot
+        self
     }
 
     pub fn position(&self) -> (isize, isize) {
