7153c07f5ecbac2a03711c1b5bf6c2ad2eaf88a3
Binsheng Liu <lbsx@users.noreply.github.com>
2017-09-21 17:04:09 +0800

    solve sublist with naive comparison

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -28,44 +28,21 @@ pub fn sublist<T: PartialEq + Debug + Display>(
     Comparison::Unequal
 }
 
-// Dp approach. Is l1 a sublist of l2
+// Is l1 a sublist of l2
 fn is_sublist<T: PartialEq + Debug + Display>(l1: &[T], l2: &[T]) -> bool {
+    assert!(l1.len() <= l2.len());
+
     if l1.is_empty() {
         return true;
     }
 
-    let len_l1 = l1.len();
-    let len_l2 = l2.len();
-
-    let mut dp = Vec::<usize>::with_capacity(len_l2 + 1);
-    dp.resize(len_l2 + 1, 0);
-
-    for row in 1..(len_l1 + 1) {
-        let mut new_dp = Vec::<usize>::with_capacity(len_l2 + 1);
-        new_dp.resize(len_l2 + 1, 0);
-        let mut has_equal_in_this_row = false;
-        for col in 1..(len_l2 + 1) {
-            if len_l2 - col < len_l1 - row && !has_equal_in_this_row {
-                return false;
-            }
-
-            if l1[row - 1] == l2[col - 1] {
-                has_equal_in_this_row = true;
-                new_dp[col] = dp[col - 1] + 1;
-            }
-
-            if new_dp[col] == len_l1 {
-                return true;
-            }
+    for (l2_index, l2_elem) in
+        l2.iter().take(l2.len() - l1.len() + 1).enumerate()
+    {
+        if l1[0] == *l2_elem && l1 == &l2[l2_index..l2_index + l1.len()] {
+            return true;
         }
-
-        if !has_equal_in_this_row {
-            return false;
-        }
-
-        dp = new_dp;
     }
-
 
     false
 }
