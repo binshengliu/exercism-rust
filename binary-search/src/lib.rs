a0cfc354dd7594eb3a8274d28ee3a15c8b78db5d
Binsheng Liu <lbsx@users.noreply.github.com>
2017-11-08 16:56:51 +0800

    use AsRef to accept more types

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,7 +1,23 @@
-pub fn find<T>(list: &[T], value: T) -> Option<usize>
+// See
+// https://github.com/rust-lang/rust/issues/24140#issuecomment-90626264
+// for difference between Borrow and AsRef.
+//
+// Borrow only converts a T to &T. For example, vec![1, 2] can only be
+// converted to &Vec, not to &[1, 2].
+//
+// AsRef can convert multiple types to &T. For example, vec![1, 2],
+// [1, 2], and &[1, 2], can both be converted to &[1, 2].
+//
+// Also see the signature of `fn open<P: AsRef<Path>>(path: P) ->
+// Result<File>`. Any type that can be converted to &Path is accepted
+// by the function.
+
+pub fn find<T, A>(list: A, value: T) -> Option<usize>
 where
     T: PartialOrd,
+    A: AsRef<[T]>,
 {
+    let list = list.as_ref();
     if list.is_empty() {
         return None;
     }
