e15f574b9436ee562c5cb3a5a3306991efd82b55
Binsheng Liu <lbsx@users.noreply.github.com>
2017-11-30 22:45:23 +0800

    solve book-store using vec

--- book-store/src/lib.rs
+++ book-store/src/lib.rs
@@ -1,44 +1,40 @@
 extern crate itertools;
 
-use std::collections::HashMap;
 use itertools::Itertools;
 
 const BOOK_PRICE: f64 = 8.0;
 const DISCOUNT: &[f64] = &[1.0, 0.95, 0.9, 0.8, 0.75];
 
 pub fn lowest_price(books: &[usize]) -> f64 {
-    let mut map: HashMap<usize, usize> = HashMap::new();
-    for &book in books.iter() {
-        *map.entry(book).or_insert(0) += 1;
-    }
-
-    low_price(&map)
-}
-
-pub fn low_price(books: &HashMap<usize, usize>) -> f64 {
     if books.is_empty() {
         return 0.0;
     }
 
-    if books.iter().all(|(_, &v)| v == 1) {
-        let count = books.len();
-        return BOOK_PRICE * count as f64 * DISCOUNT[count - 1];
+    let mut stats = vec![0; 5];
+    for &book in books.iter() {
+        stats[book - 1] += 1;
     }
 
-    if books.len() == 1 {
-        return BOOK_PRICE * *books.values().next().unwrap() as f64;
+    low_price(&stats)
+}
+
+pub fn low_price(stats: &[usize]) -> f64 {
+    let types = stats.iter().filter(|&&c| c != 0).count();
+    if stats.iter().all(|&c| c == 1 || c == 0) {
+        return BOOK_PRICE * types as f64 * DISCOUNT[types - 1];
+    }
+
+    if stats.iter().filter(|&&c| c != 0).count() == 1 {
+        return BOOK_PRICE * stats.iter().sum::<usize>() as f64;
     }
 
     let mut lowest: Option<f64> = None;
     for count in 2..6 {
-        for comb in books.keys().combinations(count) {
+        for comb in (1..6).filter(|&c| stats[c - 1] != 0).combinations(count) {
             let mut price = BOOK_PRICE * count as f64 * DISCOUNT[count - 1];
-            let mut remain = books.clone();
+            let mut remain = stats.to_vec();
             for book in comb {
-                *remain.get_mut(book).unwrap() -= 1;
-                if remain[book] == 0 {
-                    remain.remove(book);
-                }
+                remain[book - 1] -= 1;
             }
             price += low_price(&remain);
             lowest = lowest.map_or(Some(price), |l| Some(l.min(price)));
