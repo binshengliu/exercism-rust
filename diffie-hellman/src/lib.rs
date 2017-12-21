74dfeb99080acfd88025c709ee87da7272b03e53
Binsheng Liu <lbsx@users.noreply.github.com>
2017-12-21 22:36:35 +0800

    solve diffie-hellman

---
+++ diffie-hellman/src/lib.rs
@@ -0,0 +1,15 @@
+extern crate rand;
+
+use rand::Rng;
+
+pub fn private_key(p: u64) -> u64 {
+    rand::thread_rng().gen_range(2, p)
+}
+
+pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
+    g.pow(a as u32) % p
+}
+
+pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
+    b_pub.pow(a as u32) % p
+}
